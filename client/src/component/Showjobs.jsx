6179f6356e03ea992769758d9f2af7ef7bfb3ecb
Milind <milindanand74@gmail.com>
2021-11-14 18:49:59 +0530

    css inlineing

--- client/src/component/Showjobs.jsx
+++ client/src/component/Showjobs.jsx
@@ -1,6 +1,5 @@
 import React,{useState,useEffect} from 'react'
 import axios from "axios"
-
 const Showjobs = () => {
     const[jobs,setJobs]=useState([])
     const[search,SetSearch]=useState("")
@@ -25,6 +24,7 @@ const Showjobs = () => {
         setJobs(filterData)
 
     }
+
    
     return (
         <>
@@ -37,13 +37,15 @@ const Showjobs = () => {
 
             {jobs.map((value)=>{
                 return(
-                <div>
-                <p>{value.title}</p>
-                <p>{value.description}</p>
-                <p>{value.external}</p>
-                <p>{value.level}</p>
-                <p>{value.publicatiob_date}</p>
-                <p>{value.location}</p>
+                <div style={{display:"grid",gridTemplateColumns:"50%",border:"1px solid black",
+                width:"50%",margin:"auto","marginTop":"100px",fontFamily:"sans-serif",fontSize:"20px"
+                }}>
+                <p > title:{value.title}</p>
+                <p>description:{value.description}</p>
+                <p>external:{value.external}</p>
+                <p>level:{value.level}</p>
+                <p>publicatiob_date:{value.publicatiob_date}</p>
+                <p>location:{value.location}</p>
              </div>
                 )
                 })
