671e94c5e9dc905ea494cbbe91abd6e9842e3fbb
Milind <milindanand74@gmail.com>
2021-11-14 17:54:55 +0530

    backend is accessed

--- client/src/component/Showjobs.jsx
+++ client/src/component/Showjobs.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import axios from "axios"
 
 const Showjobs = () => {
+    const[jobs,setJobs]=useState([])
+    console.log(jobs);
     const get=async()=>{
+        try{
         var res=await axios.get("http://localhost:8000/job")
-        console.log(res)
+        setJobs(res.data.job)
+        }
+        catch(err){
+            console.log(err);
+        }
     }
-    get()
+    useEffect(() => {
+       get()
+    }, [])
+   
     return (
+        <>
         <div>
+            <div className="search">
+
+            <input type="text" />
+            <button>Search</button>
+            </div>
+
+            {jobs.map((value)=>{
+                return(
+                <div>
+                <p>{value.title}</p>
+                <p>{value.description}</p>
+                <p>{value.external}</p>
+                <p>{value.level}</p>
+                <p>{value.publicatiob_date}</p>
+                <p>{value.location}</p>
+                </div>
+                )
+                })
+            }
             
         </div>
+        </>
     )
 }
 
